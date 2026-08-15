c043b192e21098a568d3d93e14e9108ce571a062
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-08-16 06:12:20 +0800

    fix(main): ensure logger configuration is set for Fastify application

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { logger: ['error', 'warn'] },
   );
 
   // Buat folder uploads (sama seperti sebelumnya)
